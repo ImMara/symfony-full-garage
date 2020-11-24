4dedbcc39b8cb4c6d6a595d26e0eca9be24047af
maracax <alaneraerts@live.be>
2020-11-24 19:10:54 +0100

    form success

---
+++ src/Form/CatalogueType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Car;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CatalogueType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('marque')
+            ->add('modele')
+            ->add('cover')
+            ->add('km')
+            ->add('prix')
+            ->add('proprios')
+            ->add('cylindre')
+            ->add('puissance')
+            ->add('carburant')
+            ->add('dateMiseCirculation')
+            ->add('transmission')
+            ->add('description')
+            ->add('options')
+            ->add('slug')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Car::class,
+        ]);
+    }
+}
